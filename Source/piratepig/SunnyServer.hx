afc85ace2aad0afb0cc25edb7a09aabd4e465ff1
Corey Foster <corey.foster@gmail.com>
2014-04-26 23:34:13 +0900

    fixes

--- Source/piratepig/SunnyServer.hx
+++ Source/piratepig/SunnyServer.hx
@@ -1,6 +1,8 @@
 package piratepig;
 
+import piratepig.SunnyServer.ClientAction;
 import haxe.Serializer;
+import haxe.Unserializer;
 import haxe.ds.IntMap;
 import neko.Lib;
 import neko.net.ThreadServer;
@@ -12,7 +14,7 @@ import neko.net.ThreadServer;
 // Client Actions
 // ============================================================================
 
-abstract ClientAction {
+class ClientAction {
 	public function doHandle(c : Client) {
 	}
 }
@@ -22,14 +24,14 @@ class StartMoving extends ClientAction {
 	var velocity : Int;
 
 	public override function doHandle(c : Client) {
-		c.direction = direction;
-		c.velocity = velocity;
+		c.set_direction(direction);
+		c.set_velocity(velocity);
 	}
 }
 
 class StopMoving extends ClientAction {
 	public override function doHandle(c : Client) {
-		c.velocity = 0;
+		c.set_velocity(0);
 	}
 }
 
@@ -42,13 +44,71 @@ enum ClientStatus {
 	Disconnected;
 }
 
-typedef Client = {
-	var id : Int;
-	var x : Int;
-	var y : Int;
-	var direction : Int;
-	var velocity : Int; // pixels-per-second
-	var status : ClientStatus;
+class Client {
+	@:isVar private var id(get_id, set_id): Int;
+	@:isVar private var x(get_x, set_x): Int;
+	@:isVar private var y(get_y, set_y): Int;
+	@:isVar private var direction(get_direction, set_direction): Int;
+	@:isVar private var velocity(get_velocity, set_velocity): Int; // pixels-per-second
+	@:isVar private var status(get_status, set_status): ClientStatus;
+
+	public function new(id: Int, x: Int, y: Int, direction: Int, velocity: Int, status: ClientStatus) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.direction = direction;
+		this.velocity = velocity;
+		this.status = status;
+	}
+
+	public function set_id(value:Int) {
+		return this.id = value;
+	}
+
+	public function get_id():Int {
+		return id;
+	}
+
+	public function set_x(value:Int) {
+		return this.x = value;
+	}
+
+	public function get_x():Int {
+		return x;
+	}
+
+	public function set_y(value:Int) {
+		return this.y = value;
+	}
+
+	public function get_y():Int {
+		return y;
+	}
+
+	public function set_direction(value:Int) {
+		return this.direction = value;
+	}
+
+	public function get_direction():Int {
+		return direction;
+	}
+
+	public function get_velocity():Int {
+		return velocity;
+	}
+
+	public function set_velocity(value:Int) {
+		return this.velocity = value;
+	}
+
+	public function get_status():ClientStatus {
+		return status;
+	}
+
+	public function set_status(value:ClientStatus) {
+		return this.status = value;
+	}
+
 }
 
 typedef Message = {
@@ -143,18 +203,12 @@ class SunnyServer extends ThreadServer<Client, Message> {
 
 	// create a client
 	override public dynamic function clientConnected(s:sys.net.Socket) : Client {
-		var client : Client = new Client();
-		client.id = _nextClientId;
-		client.x = Std.random(100)+1;
-		client.y = Std.random(100)+1;
-		client.direction = 0;
-		client.velocity = 0;
-		client.status = ClientStatus.Connected;
+		var client : Client = new Client(_nextClientId, Std.random(100)+1, Std.random(100)+1, 0, 0, ClientStatus.Connected);
 
 		_nextClientId++;
 		Lib.println("client " + client.id + " is " + s.peer());
 		sendData(s, haxe.Serializer.run(client.id));
-		return { client : Client };
+		return client;
 	}
 
 	override public dynamic function clientDisconnected(c:Client) {
@@ -179,7 +233,8 @@ class SunnyServer extends ThreadServer<Client, Message> {
 		// got a full message, return it
 		var msg:String = buf.getString(pos, cpos-pos);
 
-		Unserializer.run(msg);
+		var clientAction : ClientAction = haxe.Unserializer.run(msg);
+		clientAction.doHandle(_clients.get(c.id));
 
 		return {msg: {str: msg}, bytes: cpos-pos};
 	}
